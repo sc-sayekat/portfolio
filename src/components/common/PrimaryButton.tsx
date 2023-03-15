84cf013e192006cabab082874d4f866e0c3f0b48
Foysal Ahmed <foysalxahmed@gmail.com>
2023-03-15 10:24:48 +0600

    Update button outline when in focus

--- src/components/common/PrimaryButton.tsx
+++ src/components/common/PrimaryButton.tsx
@@ -10,7 +10,7 @@ const PrimaryButton: FC<Props> = ({ children, onClick }) => {
     <button
       type='button'
       onClick={onClick}
-      className='rounded-full bg-gradient-to-r from-primary via-primary to-primary px-5 py-2.5 text-center text-sm font-medium uppercase tracking-widest text-white transition-all duration-300 hover:bg-gradient-to-br focus:outline-none focus:ring-4 focus:ring-red-300'>
+      className='rounded-full bg-gradient-to-r from-primary via-primary to-primary px-5 py-2.5 text-center text-sm font-medium uppercase tracking-widest text-white ring-offset-0 transition-all duration-100 hover:bg-gradient-to-br focus:ring-2 focus:ring-slate-200 '>
       {children}
     </button>
   )
